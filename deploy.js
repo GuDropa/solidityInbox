60c9acb7b3c21d2815a64c8dc079dbb8ab16127d
grandxdropa <dropa@grandx.com.br>
2022-03-12 16:51:30 -0300

    deploy file with provider created

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
 // deploy code will go here
+const HDWaleltProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWaleltProvider(
+  // mnemonic,
+  // infura api link 
+);
+
+const web3 = new Web3(provider);
+
